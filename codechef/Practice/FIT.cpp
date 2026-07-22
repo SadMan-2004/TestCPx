f65b52c91f6bf441367dfd359de73d632de0b508
SadMan-2004 <aa9334162@gmail.com>
2026-07-23 02:35:06 +0600

    Solved: FIT (codechef)

---
+++ codechef/Practice/FIT.cpp
@@ -0,0 +1,20 @@
+// Problem: FIT
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-math/BASICMATH/problems/FIT
+// Solved on: 2026-07-22T20:35:05.757Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        // your code goes here
+        cout << x * 10 << '\n';
+    }
+}
